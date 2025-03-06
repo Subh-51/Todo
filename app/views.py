97b3077b03b88944a586ab9fccd306a9166c8634
unknown <sradhanjali.seema.7@gmail.com>
2025-03-06 17:45:18 +0530

    file add

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
@@ -11,3 +13,15 @@ def home(request):
         TO = Todo(title=title, desc=desc)
         TO.save()
      return render(request,'home.html',d)
+def update(request, pk):
+    T = Todo.objects.get(pk=pk)
+    d = {'T': T}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        desc = request.POST.get('desc')
+        T.title = title
+        T.desc = desc
+        T.save()
+        return HttpResponseRedirect(reverse('home'))
+    return render(request, 'update.html', d)
+
